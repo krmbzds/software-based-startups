318eb860460fb5f8b8c06567c43427c04a88fe24
Kerem Bozdas <krmbzds.github@gmail.com>
2014-05-15 18:05:23 +0300

    Add web.js to load index.html

---
+++ week11/bitstarter/web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var buf = fs.readFileSync('index.html');
+  var content = buf.toString();
+  response.send(content);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
